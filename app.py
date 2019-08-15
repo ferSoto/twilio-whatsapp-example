11694a1924a109b2ac93809a6170a9546947f299
Fernando Soto <zomaotoko@gmail.com>
2019-08-15 16:50:16 -0500

    Receive media
    Also, consumes a service.

--- app.py
+++ app.py
@@ -11,9 +11,25 @@ from twilio.rest import Client
 SID = os.environ['TWILIO_SID']
 AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
+# This URL calls a service which extract text from an image or a PDF file.
+# The endpoint `inpsect` receives a POST request with form data and a single
+# parameter 'url' which contains the URL to the media file.
+EXTRACTOR_URL = 'http://0.0.0.0:5042/inspect'
+
 
 client = Client(SID, AUTH_TOKEN)
 app = Flask(__name__)
+
+
+def media_content(url):
+    """ Calls a service which extracts text from images and PDF files.
+
+    :param String url: URL to Image/PDF file.
+    """
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {'url': url}
+    response = requests.post(EXTRACTOR_URL, headers=headers, data=data)
+    return response.content
 
 
 @app.route('/whatsapp', methods=['POST'])
@@ -22,7 +38,18 @@ def whatsapp_entry():
     _from = request.form['From']
     # Twilio sandbox number
     _to = request.form['To']
-    message = 'Holi, crayoli!'
+    # Number of media files. As far as I know, it's always 1 or 0.
+    # If more than one file is sent, this endpoint will receive two or more
+    # requests.
+    num_media = int(request.form['NumMedia'])
+    message = None
+    if num_media > 0:
+        # Process media (Image or PDF)
+        url = request.form['MediaUrl0']
+        message = media_content(url)
+    else:
+        # Reply with generic message
+        message = 'Holi, crayoli!'
 
     return str(client.messages.create(
         to=_from,
